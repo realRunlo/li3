ae57672e63532c9c9452ffee7c6bf924a646d64e
realRunlo <gteixeira.pereira@gmail.com>
2021-03-26 15:45:12 +0000

    .h das structs

---
+++ project_c/business.h
@@ -0,0 +1,14 @@
+#ifndef ___BUSINESS_H___
+#define ___BUSINESS_H___
+
+
+typedef struct business{
+    char * business_id;
+    char * name;
+    char * city;
+    char * state;
+    char * categories;
+
+} * Business;
+
+#endif //___BUSINESS_H___
---
+++ project_c/user.h
@@ -0,0 +1,16 @@
+#ifndef ___USER_H___
+#define ___USER_H___
+
+typedef struct user{
+    char * user_id;
+    char * name;
+    char * friends;
+}* User;
+
+
+
+
+#endif //___USER_H___
+
+
+
