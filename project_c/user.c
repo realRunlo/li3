ea65425f27fe4b97b9b457aa2b38ca6c51c96d18
brazafonso <61985783+brazafonso@users.noreply.github.com>
2021-03-29 18:44:03 +0100

    pequenas alteracoes
    algumas melhorias nos getters e setters e testes com GHashTable

--- project_c/user.c
+++ project_c/user.c
@@ -18,39 +18,58 @@ char *strsep(char **stringp, const char *delim) {
 
 
 char * getUserId(User u){
-    char * newUserId = malloc(sizeof(u->user_id));
-    newUserId = strcpy(newUserId,u->user_id);
-    return newUserId;
+    return strdup(u->user_id);
+}
+
+void setUserId(User u, char* newUserId){
+    //printf("%s\n",newUserId);
+    u->user_id = strdup(newUserId);
 }
 
 
 char * getName(User u){
-    char * newName = malloc(sizeof(u->name));
-    newName = strcpy(newName,u->name);
-    return newName;
+    return strdup(u->name);
+}
+
+void setName(User u, char* newName){
+    u->name = strdup(newName);
 }
 
 char * getFriends(User u){
-    char * newFriends = malloc(sizeof(u->friends));
-    newFriends = strcpy(newFriends,u->friends);
-    return newFriends;
+
+    return strdup(u->friends);
+}
+
+void setFriends(User u, char* newFriends){
+    u->friends = strdup(newFriends);
 }
 
 
-User * UserList(){
+User * userList(){
     User* list = malloc(sizeof(User));
     list[0] = NULL;
     return list;
 }
 
 
+void printUserList(User * uList){
+    for(int n = 0; uList[n] != NULL;n++){
+        //printf("%d : %s;\n",n,getUserId(uList[n]));
 
-void addUser(User* uList, char * buffer, int n){
-    uList[n] = malloc(sizeof(User));
-    uList[n]->user_id = strdup(strsep(&buffer,";"));
-    uList[n]->name = strdup(strsep(&buffer,";"));
-    uList[n]->friends = strdup(strsep(&buffer,";"));
-    printf("%d : %s;%s;%s\n",n,uList[n]->user_id,uList[n]->name,uList[n]->friends);
+        printf("%d : %s;%s;%s\n",n,getUserId(uList[n]),getName(uList[n]),getFriends(uList[n]));
+
+    }
+}
+
+
+void addUser(User* u, char * buffer, int n){
+    u[n] = malloc(sizeof(struct user));
+
+
+    setUserId(u[n],strsep(&buffer,";"));
+    setName(u[n],strsep(&buffer,";"));
+    setFriends(u[n],strsep(&buffer,"\n"));
+
 }
 
 
@@ -65,40 +84,63 @@ User* readUser(char * filename){
     }
 
     char buffer[300];
-    User* Ulist = UserList();
+    User* ulist = userList();
     int i = 0;
 
     while(fgets(buffer,300,f)){
-        addUser(Ulist,buffer,i);
+        printf("%d : %s\n",i,buffer);
+        addUser(ulist,buffer,i);
         i++;
     }
+    ulist[i] = malloc(sizeof(User));
+    ulist[i] = NULL;
     fclose(f);
 
-    return Ulist;
+    return ulist;
 }
 
+void iterator(gpointer key, gpointer value, gpointer user_data) {
+ printf(user_data, key,getName((User) value),getFriends((User) value));
+}
 
 
 //main to test functions of user.c for now
 int main(){
-    char filename[100] ="./input_files/users_full.csv";
-
+    char filename[100] ="./input_files/users_teste.csv";
 /*
     printf("Indique o ficheiro a ler.\n");
     scanf("%s",filename);
 */
-    User* list = readUser(filename);
 
+
+    User* uList = readUser(filename);
+    printUserList(uList);
 /*
+    User u = malloc(sizeof(struct user));
+    setUserId(u,"primeiro");
+    setName(u,"goncalo");
+    setFriends(u,"ze,runlo,nuno");
+
+    GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+    g_hash_table_insert(hash, GINT_TO_POINTER((getUserId(u))),u);
+    g_hash_table_foreach(hash, (GHFunc)iterator, "%s;%s;%s;\n");
+
+
+
 //testes das funcoes
-    User u = malloc(sizeof(User));
-    u->user_id = "123456";
-    u->name = "Goncalo";
-    u->friends = "Joaquim,Jose,Pedro";
+/*
+    User *list = userList();
+    list[0] = malloc(sizeof(User));
+    setUserId(list[0],"122341");
+    setName(list[0],"goncalo");
+    setFriends(list[0],"joaquim,jose,pedro");
+    list[1]=malloc(sizeof(User));
+    list[1] = NULL;
+    printUserList(list);
 
     printf("string original :%d\n",u->user_id);
     printf("string copiada :%d\n",getUserId(u));
 */
 
-
+    return 0;
 }
