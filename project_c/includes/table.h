09e8cc97de3bd58b4d985d20b50bf1040dd1ac87
realRunlo <gteixeira.pereira@gmail.com>
2021-04-20 14:02:09 +0100

    paginção(faltas + testes),index_table

--- project_c/includes/table.h
+++ project_c/includes/table.h
@@ -13,8 +13,11 @@ void setNewLine(TABLE t,char * line);
 int getEntries(TABLE t);
 char* get_string_table(TABLE t,int n);
 char** getTab(TABLE t);
+
 int getBlen_str(TABLE t);
 void printPage_table(TABLE t,int current_page);
+void print_Table(TABLE t);
+TABLE index_table(TABLE t,int line,int col);
 
 
 
--- project_c/src/table.c
+++ project_c/src/table.c
@@ -86,7 +86,7 @@ void printPage_table(TABLE t,int current_page){
     int total_entries = getEntries(t);
     int total_pages = total_entries/MAXTPAGE;
     int bottom = current_page*MAXTPAGE;
-    int top = current_page*MAXTPAGE+MAXTPAGE;
+    int top = bottom+MAXTPAGE;
 
     int space_in_line = getBlen_str(t) + MARGIN;
     char * seped;
@@ -107,7 +107,48 @@ void printPage_table(TABLE t,int current_page){
         printf("\n");
     }
     print_LineTops(space_in_line);
-    printf("Page %d out of %d\n",current_page,total_pages);
+    printf("Page %d out of %d\n",current_page+1,total_pages+1);
+}
+
+void print_Table(TABLE t){
+    int space_in_line = getBlen_str(t) + MARGIN;
+    char * seped;
+    for(int i=0;i<getEntries(t);i++){
+        char * str_zero = strdup(t->tab[i]);
+        print_LineTops(space_in_line);
+        printf("|");
+        while((seped = strsep(&str_zero,";"))){
+        int str_length = strlen(seped);
+        int space_left = (space_in_line-str_length)/2;
+        int space_rigth = (space_in_line-space_left-str_length);
+
+        printN_space(space_left);
+        printf("%s",seped);
+        printN_space(space_rigth);
+        printf("|");
+        }
+        printf("\n");
+    }
+    print_LineTops(space_in_line);
+
+}
+
+TABLE index_table(TABLE t,int line,int col){
+    TABLE indexed_table = init_Sized_Table(2);
+    char * headLine = get_string_table(t,0);
+    char * seped;
+    for(int i=0;i<col+1;i++){
+        seped=strsep(&headLine,";");
+    }
+    setNewLine(indexed_table,strdup(seped));
+
+    char * strLine = get_string_table(t,line+1);
+     for(int i=0;i<col+1;i++){
+        seped=strsep(&strLine,",");
+    }
+    setNewLine(indexed_table,strdup(seped));
+
+    return indexed_table;
 }
 
 
